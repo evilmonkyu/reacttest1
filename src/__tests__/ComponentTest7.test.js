34acc6028e04a2e0796bb088ab24932d99cfb071
Greg West <greg.west@axa-ppp.co.uk>
2019-07-23 15:55:00 +0100

    More tests

---
+++ src/__tests__/ComponentTest7.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import ComponentTest from "../components/ComponentTest";
+
+describe("A test 6 component", () => {
+  it("Renders just fine", () => {
+    let component = mount(<ComponentTest />);
+  });
+
+  for (var i = 2; i <= 800; i++) {
+    it("Renders just fine " + i, () => {
+      let component = mount(<ComponentTest val={i} />);
+      expect(component.find("div").text()).toBe("" + i);
+    });
+  }
+});
---
+++ src/components/C4.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class C extends Component {
+  render() {
+    return <div />;
+  }
+}
